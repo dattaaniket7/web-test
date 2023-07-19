607cc6e28457206df6b2781bbc6a2ac698e04392
Aniket Datta <datta.aniket7@gmail.com>
2023-07-20 01:19:03 +0530

    bind

--- App.js
+++ App.js
@@ -1,1 +1,19 @@
-console.log(this)
+function getUserDetails () {
+    console.log(`My name is ${this.name} and my age is ${this.age}`)
+}
+
+const oldMonk = {
+    age: 7,
+    name: "oldMonk",
+    description: "Old Monk XXX 7 Years Old"
+}
+
+const aniket = {
+    age: 23,
+    name: "Aniket Datta",
+    aboutMe: "I love Old Monk"
+}
+
+const oldMonkDetail = getUserDetails.bind(oldMonk)
+
+oldMonkDetail()
