e68450e4b994b82f23cd13cd735391477807c609
Aniket Datta <datta.aniket7@gmail.com>
2023-07-20 02:02:44 +0530

    fadeOut

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
-const user = {
-    name: "oldMonk",
-    regularFunction: function () {
-        console.log(this.name)
-    },
-    arrowFunction: () => {
-        console.log(this.name)
-    },
+const $ = function (selector) {
+    const elements = document.querySelectorAll.call(document, selector)
+
+    //actual library functionality
+    const library = {
+        elements,
+        addClass(className) {
+            this.elements.forEach((element) => {
+                element.classList.add(className)
+            })
+            return library
+        },
+        //add fadeout
+        fadeOut(duration) {
+            this.elements.forEach((element) => {
+                let currentOpacity = 1
+                const decrement = 1 / (duration / 10)
+
+                const fade = setInterval(() => {
+                    currentOpacity -= decrement
+                    element.style.opacity = currentOpacity
+
+                    if (currentOpacity <= 0) {
+                        clearInterval(fade)
+                        element.style.display = "none"
+                    } 
+                }, 10)
+            })
+        return library
+        },
+    }
+    return library
 }
 
-user.regularFunction()
-user.arrowFunction()
+$("p").addClass("fade").fadeOut(1000)
