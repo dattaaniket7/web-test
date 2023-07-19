5cf08afe859ea845bcadb8f8fe12e2b3d54f7050
Aniket Datta <datta.aniket7@gmail.com>
2023-07-20 01:32:26 +0530

    arrow function

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
-class User {
-    constructor(name, age) {
-        this.name = name
-        this.age = age
-    }
-    describe() {
-        console.log(`${this.name} is now a user on this website and his age is ${this.age}`)
-    }
+const user = {
+    name: "oldMonk",
+    regularFunction: function () {
+        console.log(this.name)
+    },
+    arrowFunction: () => {
+        console.log(this.name)
+    },
 }
 
-const user = new User("oldMonk", 7)
-
-user.describe()
+user.regularFunction()
+user.arrowFunction()
